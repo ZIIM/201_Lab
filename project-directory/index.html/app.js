7bf13f89ba2af9776de2a35d99c56a219ec1216a
ALEJANDRA ALTAMIRANO <a.altamirano0208@gmail.com>
2023-07-13 16:43:59 -0700

    updated js to be dry

--- project-directory/index.html/app.js
+++ project-directory/index.html/app.js
@@ -15,108 +15,73 @@ const questions = [
   'Do dogs have tails? (y/n)',
   'Can birds fly? (y/n)',
   'Is the Earth flat? (y/n)',
-  'Is JavaScript a programming language? (y/n)'
+  'Is JavaScript a programming language? (y/n)',
+  'Guess a number between 1 and 10.',
+  'What is one of my favorite colors?'
 ];
 const answers = [
-  'y',
-  'y',
-  'y',
-  'n',
-  'y'
+  ['y','yes'],
+  ['y','yes'],
+  ['y', 'yes'],
+  ['n', 'no'],
+  ['y','yes'],
+  [''],
+  ['red','green','blue','black', 'white']
 ];
 
-let response1 = prompt(questions[0]);
-if (response1.toLowerCase()[0] === answers[0]) {
-  alert('Correct!');
-  userScore++;
-} else {
-  alert('Sorry thats incorrect.');
-}
-alert(`Current score: ${userScore}`);
-
-let response2 = prompt(questions[1]);
-if (response2.toLowerCase()[0] === answers[1]) {
-  alert('Correct!');
-  userScore++;
-} else {
-  alert('Sorry thats incorrect.');
-}
-alert(`Current score: ${userScore}`);
-
-let response3 = prompt(questions[2]);
-if (response3.toLowerCase()[0] === answers[2]) {
-  alert('Correct!');
-  userScore++;
-} else {
-  alert('Sorry thats incorrect.');
-}
-alert(`Current score: ${userScore}`);
-
-let response4 = prompt(questions[3]);
-if (response4.toLowerCase()[3] === answers[3]) {
-  alert('Correct!');
-  userScore++;
-} else {
-  alert('Sorry thats incorrect.');
-}
-alert(`Current score: ${userScore}`);
-
-let response5 = prompt(questions[4]);
-if (response5.toLowerCase()[0] === answers[4]) {
-  alert('Correct!');
-  userScore++;
-} else {
-  alert('Sorry thats incorrect.');
-}
-alert(`Current score: ${userScore}`);
-// NUUMBER GUESSING GAME pt.1
-const randomNumber = Math.floor((Math.random()* 10)) + 1;
-
-  const MAX_NUMBER_GUESSES = 4; 
-  const MAX_TRIVIA_GUESSES = 6; 
-  let numNumberGuesses = 0;
-  let numTriviaGuesses = 0;
-  let correctNumberAnswer = false;
-  let correctTriviaAnswer = false;
-  let userAnswer = '';
+function askQuestion(question, answer, maxAttempts) {
+  let remainingAttempts = maxAttempts;
   let hint = '';
-
-  console.log('Shh, don\'t tell anyone, but this is my secret number: ' + randomNumber);
-
-  // NUMBER GUESSING GAME pt.2
-  do {
-    userAnswer = prompt(hint + 'Guess a number between 1 and 10. ' + (MAX_NUMBER_GUESSES - numNumberGuesses) + ' guesses remaining.');
-    userAnswer = parseInt(userAnswer);
-    numNumberGuesses += 1;
-    if (userAnswer === randomNumber) {
-      correctNumberAnswer = true;
-    } else if (userAnswer < randomNumber) {
-      hint = 'Too low\n\n';
-    } else {
-      hint = 'Too high\n\n';
+  if (question.indexOf('Guess a number between 1 and 10.') !== -1) {
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    console.log('Shh, don\'t tell anyone, but this is my secret number: ' + randomNumber);
+    while (remainingAttempts > 0) {
+      let response = prompt(hint + question + ` (${remainingAttempts} attempts remaining)`);
+      response = parseInt(response);
+      remainingAttempts--;
+      let isCorrect = false;
+      if (response === randomNumber) {
+        isCorrect = true;
+      } else if (response < randomNumber) {
+        hint = 'Too low\n\n';
+      } else if (response > randomNumber) {
+        hint = 'Too high\n\n';
+      }
+      if (isCorrect) {
+        alert('Correct!');
+        userScore++;
+        break;
+      } else if (remainingAttempts > 0) {
+        alert('Incorrect! Try again.');
+      } else {
+        alert(`Sorry, you are out of attempts. The secret number was ${randomNumber}.`);
+      }
     }
-  } while (numNumberGuesses < MAX_NUMBER_GUESSES && !correctNumberAnswer);
-
-  if (correctNumberAnswer) {
-    alert('You are correct!')
   } else {
-    alert('All wrong. The number was: ' + randomNumber);
+    while (remainingAttempts > 0) {
+      let response = prompt(question + ` (${remainingAttempts} attempts remaining)`);
+      let isCorrect = false;
+      for (let i = 0; i < answer.length; i++) {
+        if (response.toLowerCase() === answer[i].toLowerCase()) {
+          isCorrect = true;
+          break;
+        }
+      }
+      if (isCorrect) {
+        alert('Correct!');
+        userScore++;
+        break;
+      } else if (remainingAttempts > 0) {
+        alert(`Sorry, that's incorrect. ${remainingAttempts} attempts remaining.`);
+      } else {
+        alert('Sorry, you are out of attempts.');
+      }
+      remainingAttempts--;
+    }
   }
-
-
-// ARRAY QUESTION
-const multipleGuessQuestion = 'What is one of my favorite colors?';
-const multipleGuessAnswers = ['red', 'green', 'blue', 'black', 'white'];
-
-for (let attempts = 6; attempts > 0; attempts--) {
-  let response = prompt(multipleGuessQuestion);
-  if(multipleGuessAnswers.includes(response)) {
-    userScore++;
-    alert('Correct!');
-    break;
-  } else {
-    alert(`Incorrect, you have ${attempts} attempts left`);
-  }
+  alert(`Current score: ${userScore}`);
 }
 
-alert(`Game over! Here's your score: ${userScore}`);
+for (let i = 0; i < questions.length; i++) {
+  askQuestion(questions[i], answers[i], i === 5 ? 4 : i === 6 ? 6 : 1);
+}
